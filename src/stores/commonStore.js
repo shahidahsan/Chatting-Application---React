4d81e79c9aa15181302b49a1e4426cec16fd9bfa
Ahsan Shahid <ahsanshahidcool@gmail.com>
2020-05-06 22:20:38 -0400

    Deployed on gh pages

--- src/stores/commonStore.js
+++ src/stores/commonStore.js
@@ -3,7 +3,7 @@ import agent from '../agent';
 
 class CommonStore {
 
-  @observable appName = 'Conduit';
+  @observable appName = 'Chatsizzle';
   @observable token = window.localStorage.getItem('jwt');
   @observable appLoaded = false;
 
